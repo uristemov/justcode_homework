7d16bb3f0c20cf07a5deb2f6660c42eca2454f14
uristemov <meiirzhan.uristemov@gmail.com>
2023-11-09 13:44:29 +0600

    adding homework 16 files

---
+++ hw16/service/internal/user/repository/user.go
@@ -0,0 +1,69 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"service/internal/user/entity"
+)
+
+func (r *Repo) CreateUser(ctx context.Context, user entity.User) error {
+	q := `
+INSERT INTO user (first_name, last_name, phone, login ,password)
+VALUES (?, ?, ?, ?, ?);
+`
+	query, args, err := sqlx.In(
+		q,
+		user.FirstName,
+		user.LastName,
+		user.Phone,
+		user.Login,
+		user.Password,
+	)
+
+	if err != nil {
+		return fmt.Errorf("query bake failed: %w", err)
+	}
+
+	_, err = r.main.ExecContext(ctx, query, args...)
+	if err != nil {
+		return fmt.Errorf("db exec query failed: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Repo) ConfirmUser(ctx context.Context, login string) error {
+
+	q := "UPDATE user SET is_confirmed=true WHERE login=?;"
+
+	_, _, err := sqlx.In(q, login)
+	if err != nil {
+		return fmt.Errorf("query bake failed: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Repo) GetUserByLogin(ctx context.Context, login string) (*entity.User, error) {
+	q := "SELECT id, first_name, last_name, phone, login, password, is_confirmed, is_deleted, created_at, updated_at FROM user WHERE login = ?"
+
+	query, args, err := sqlx.In(q, login)
+	if err != nil {
+		return nil, fmt.Errorf("query bake failed: %w", err)
+	}
+
+	var row entity.User
+
+	if err := r.replica.GetContext(ctx, &row, query, args...); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNotFound
+		}
+
+		return nil, fmt.Errorf("db query failed: %w", err)
+	}
+
+	return &row, nil
+}
